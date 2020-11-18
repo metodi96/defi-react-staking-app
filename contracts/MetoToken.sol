84e76180ca436e0127d4249833358a46b238eacb
DESKTOP-TIU6GNM\Metodi <metodi.manov@tum.de>
2020-11-18 22:11:50 +0100

    add allowance, approve, transferFrom functions for the delegate transfer functionality (according to the ERC-20 standard) and the corresponding test cases

--- contracts/MetoToken.sol
+++ contracts/MetoToken.sol
@@ -7,12 +7,11 @@ contract MetoToken {
     string public standard = 'Meto Token v1.0';
     uint256 public totalSupply;
     mapping (address => uint) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
 
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    //I the owner approve the spender to spend _value amount of tokens
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -26,6 +25,31 @@ contract MetoToken {
         balanceOf[_to] += _value;
         //emit transfer event
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    //spender is the account that we want to approve to send on our behalf (e.g. address of an exchange)
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        //handle allowance and approve event
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    //delegated transfer
+    //3 accounts msg.sender, transfer from and transfer to
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        //require _from to have enough tokens and to have a big enough allowance
+        require(_value <= balanceOf[_from]);
+        //require a big enough allowance - msg.sender is approved to send on behalf of _from
+        require(_value <= allowance[_from][msg.sender]);
+        //change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //update allowance
+        allowance[_from][msg.sender] -= _value;
+        //emit Transfer event
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/metotoken.js
+++ test/metotoken.js
@@ -40,7 +40,7 @@ contract("MetoToken", accounts => {
             assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
             //to inspect the return value we need to call the transfer function
             return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
-        }).then(function(success) {
+        }).then(function (success) {
             assert.equal(success, true, 'should return true');
             return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
         }).then(function (receipt) {
@@ -57,5 +57,69 @@ contract("MetoToken", accounts => {
         }).then(function (balance) {
             assert.equal(balance.toNumber(), 9750000, 'deduction failed');
         })
+    });
+
+    it("...should approve tokens for delegated transfer.", function () {
+        return MetoToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function (success) {
+            assert.equal(success, true, 'should return true');
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0] })
+        }).then(function (receipt) {
+            //receipt is for events
+            assert.equal(receipt.logs.length, 1, 'should trigger one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'should log the owner account who initiates the approval');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'should log the spender account');
+            assert.equal(receipt.logs[0].args._value, 100, 'should log the approval amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function (allowance) {
+            assert.equal(allowance.toNumber(), 100, 'should store the allowance for a delegated transfer');
+        })
+    })
+    //side note - if some assertion fails, even if the last one is right, it shows that the last one is wrong in the console.........
+    it("...should handle delegated token transfers.", function () {
+        return MetoToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            //initial setup by transfering to fromAccount
+            return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+        }).then(function (receipt) {
+            //approve spendingAccount to spend 10 tokens from fromAccount
+            return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+        }).then(function (receipt) {
+            //try transfering something larger than the sender's balance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount })
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+            //try transfering something larger than the approved amount
+            return tokenInstance.transferFrom(fromAccount, toAccount, 11, { from: spendingAccount });
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+            //we call only for the return value
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function (success) {
+            assert.equal(success, true, 'should return true');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function (receipt) {
+            //receipt is for events
+            assert.equal(receipt.logs.length, 1, 'should trigger one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'should log the account the tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'should log the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 10, 'should log the transfer amount');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 90, 'should deduct 10 from the sending account');
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 10, 'should add 10 to the receiving account');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function (allowance) {
+            assert.equal(allowance.toNumber(), 0, 'should deduct 10 from 10 allowance');
+        });
     });
 });
