ee227cbf86fa35e6ddb78e678c52d3fdb9aebec0
DESKTOP-TIU6GNM\Metodi <metodi.manov@tum.de>
2020-12-09 01:46:49 +0100

    add functions to issue and withdraw (arbitrary amount of) tokens and corresponding test cases

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -16,12 +16,19 @@ contract TokenFarm {
     address[] public stakers;
 
     event Staked(address by, uint256 amount);
+    event Withdrawn(address by, uint256 amount);
+    event Issued(address to, uint256 amount);
 
     constructor(MetoTokenV2Farm _metoTokenV2, DaiToken _daiToken) {
         //in order to use them in other functions
         metoTokenV2 = _metoTokenV2;
         daiToken = _daiToken;
         owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner of the token farm can call this function");
+        _;
     }
 
     function getStakers() public view returns(address[] memory) {
@@ -52,7 +59,44 @@ contract TokenFarm {
     }
 
     //unstake tokens - investor withdraws money (withdraw)
+    //withdraw and arbitrary amount of tokens
+    function withdrawTokens(uint256 _amount) public {
+        require(_amount > 0, "Amount cannot be 0");
+        require(_amount <= stakingBalance[msg.sender], "Cannot withdraw more than you have in your staking balance");
+        require(hasStaked[msg.sender], "Caller must have staked in order to withdraw something");
+        //transfer mock dai tokens to this contract for staking
+
+        daiToken.transfer(msg.sender, _amount);
+
+        //update staking balance
+        stakingBalance[msg.sender] -= _amount;
+
+        //if account has withdrawn everything we need to remove them from the stakers array
+        
+        if (stakingBalance[msg.sender] == 0) {
+            address[] storage arr = stakers;
+            for (uint i=0; i < arr.length; i++) {
+                if (arr[i] == msg.sender) {
+                    arr[i] = arr[arr.length - 1];
+                    arr.pop();
+                    //update staking status
+                    hasStaked[msg.sender] = false;
+                    isStaking[msg.sender] = false;
+                    break;
+                }
+            }
+        }
+        emit Withdrawn(msg.sender, _amount);
+    }
 
     //issue tokens
-
+    function issueTokens() public onlyOwner {
+        //issue tokens which are the same amount as the tokens the user has staked
+        for (uint256 i = 0; i < stakers.length; i++) {
+            if (stakingBalance[stakers[i]] > 0 ) {
+                metoTokenV2.transfer(stakers[i], stakingBalance[stakers[i]]);
+                emit Issued(stakers[i], stakingBalance[stakers[i]]);
+            }
+        }
+    }
 }
--- test/tokenfarm.js
+++ test/tokenfarm.js
@@ -104,8 +104,106 @@ contract('TokenFarm', ([owner, investor]) => {
 
             //check stakers array
             result = await tokenFarm.getStakers();
-            console.log(`Stakers array: ${result}`)
+            //console.log(`Stakers array: ${result}`)
             assert.equal(result.length, 1, 'should have 1 address');
+        })
+    })
+
+    describe('Token Farm issuing', async () => {
+        it('should issue the same amount of tokens that the investor has staked', async () => {
+            let result;
+            // Check investor balance before staking
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor Mock DAI wallet balance correct before issuing since hes staked them already')
+
+            result = await metoToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor Meto Token wallet balance correct before being issued the new tokens')
+            
+            //only the owner can issue so this must fail
+            await truffleAssert.reverts(tokenFarm.issueTokens({ from: investor }));
+
+            //test event
+            result = await tokenFarm.issueTokens({ from: owner });
+            assert.equal(result.logs.length, 1, 'should trigger one event');
+            assert.equal(result.logs[0].event, 'Issued', 'should be the "Issued" event');
+            assert.equal(result.logs[0].args.to, investor, 'should log the addressee of the issuing');
+            assert.equal(result.logs[0].args.amount, tokens('100'), 'should log the number of staked tokens');
+
+            result = await metoToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor should possess Meto Tokens now')
+
+            result = await metoToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), tokens('999900'), 'owner should be short of 100 tokens')
+        })
+    })
+
+    describe('Token farm withdrawing', async () => {
+        it('should withdraw an arbitrary amount of tokens fewer than the staking balance', async () => {
+            let result;
+            // Check investor balance before staking
+            result = await metoToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor should be in possession of the metoTokens')
+            
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor should have 0 dai tokens')
+
+            //test if owner aka an account that hasnt staked can withdraw
+            await truffleAssert.reverts(tokenFarm.withdrawTokens(tokens('100'), { from: owner }));
+
+            //test if investor can withdraw more than he or she has
+            await truffleAssert.reverts(tokenFarm.withdrawTokens(tokens('101'), { from: investor }));
+
+            //test if investor can withdraw 0 tokens
+            await truffleAssert.reverts(tokenFarm.withdrawTokens(tokens('0'), { from: investor }));
+
+            //check isStaking and hasStaked
+            result = await tokenFarm.isStaking(investor);
+            assert.equal(result, true, "investor should be staking");
+            result = await tokenFarm.hasStaked(investor);
+            assert.equal(result, true, "investor should have staked");
+
+            //check stakers array - make sure you call the array and not just access it via .stakers because it will return a function
+            //also full arrays can be accessed only via getter, not via the state variable which sux
+            result = await tokenFarm.getStakers();
+            assert.equal(result.length, 1, 'should not be empty');
+
+            //test event
+            result = await tokenFarm.withdrawTokens(tokens('50'), { from: investor });
+            assert.equal(result.logs.length, 1, 'should trigger one event');
+            assert.equal(result.logs[0].event, 'Withdrawn', 'should be the "Withdrawn" event');
+            assert.equal(result.logs[0].args.by, investor, 'should log the caller of the function');
+            assert.equal(result.logs[0].args.amount, tokens('50'), 'should log the number of staked tokens');
+
+            result = await tokenFarm.isStaking(investor);
+            assert.equal(result, true, "investor should be staking");
+            result = await tokenFarm.hasStaked(investor);
+            assert.equal(result, true, "investor should have staked");
+
+            //check balance of investor
+            result = await daiToken.balanceOf(investor);
+            assert.equal(result, tokens('50'), 'balance should be 50 tokens now');
+
+            //check staking balance of investor
+            result = await tokenFarm.stakingBalance(investor);
+            assert.equal(result, tokens('50'), 'the staking balance should be 50 tokens')
+
+
+            result = await tokenFarm.withdrawTokens(tokens('50'), { from: investor });
+
+            //check isStaking and hasStaked
+            result = await tokenFarm.isStaking(investor);
+            assert.equal(result, false, "investor should not be staking");
+            result = await tokenFarm.hasStaked(investor);
+            assert.equal(result, false, "investor should not have staked");
+
+            //check staking balance of investor
+            result = await tokenFarm.stakingBalance(investor);
+            assert.equal(result, tokens('0'), 'the staking balance should be 0 tokens')
+
+            //check stakers array
+            result = await tokenFarm.getStakers();
+            //console.log(`Stakers array: ${result}`)
+            assert.equal(result.length, 0, 'should have 0 addresses');
         })
     })
 })
